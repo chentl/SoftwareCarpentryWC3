e3773a8844e930ffe28d676ecf500737534701d2
Tianle <chentl@icloud.com>
2019-02-17 21:20:23 -0500

    Initial commit

---
+++ mundaneMath.py
@@ -0,0 +1,3 @@
+# Assume the phrase "between 1 to 100" includes the end-point number 100
+# If not, change 101 to 100
+print(sum(range(2, 101, 2)))
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Image Processing using Pillow
+
+## blur effect and luminance adjustment
+
